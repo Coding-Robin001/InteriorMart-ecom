68e0d9b3a9f982a5db567ab6c7bdde7e756f1ab2
Coding-Robin001 <codingade@gmail.com>
2024-08-16 12:06:16 +0100

    commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -31,12 +31,16 @@ const Home = () => {
   }, [allProducts])
 
   const fetchProduct = async () => {
-    await fetch('https://interiormart-ecom-api.onrender.com/product//allProduct')
+    await fetch('https://interiormart-ecom-api.onrender.com/product/allProduct', {
+      mode: "cors"
+    })
       .then(res => res.json())
       .then(data => {
         setAllProducts(data)
         dispatch(setProducts(data))
       })
+      .catch(error => console.log(error)
+      )
   }
 
   return (
