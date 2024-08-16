4da3b1ffa1e1c1c7fe3d789c79ff5041bd77537a
Coding-Robin001 <codingade@gmail.com>
2024-08-16 13:05:57 +0100

    commit

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
   next(); // Proceed to the next middleware or route handler
 });
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
